9cdfcdf1fe927b032f1fd3fdfc03b5fe325825f0
e1b21068 <e1b21068@oit.ac.jp>
2023-10-10 14:06:56 +0900

    リポジトリの作成

---
+++ src/test/java/oit/is/apple/work03/Work03ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.apple.work03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
